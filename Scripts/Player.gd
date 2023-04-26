8c179de8897b347a9c45782242d72a8c98045610
goncalves-henrique <henrique.goncalves.001@student.uni.lu>
2023-04-26 10:44:27 +0200

    Added Henrique's Changes
    Added Henrique's Changes

---
+++ Scripts/Player.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+@export var speed = 150
+
+func _ready():
+	$AnimationPlayer.play("Stand")
+
+func _physics_process(delta):
+	velocity = Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	)
+	
+	velocity *= speed
+	
+	move_and_slide()
+	
+	if Input.is_action_pressed("ui_left"):
+		$Player.flip_h = 1
+		$AnimationPlayer.play("Walk")
+	elif Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_down") or Input.is_action_pressed("ui_up"):
+		$Player.flip_h = 0
+		$AnimationPlayer.play("Walk")
+	elif Input.is_action_just_released("ui_right") or Input.is_action_just_released("ui_down") or Input.is_action_just_released("ui_up"):
+		$Player.flip_h = 0
+		$AnimationPlayer.play("Stand")
+	elif Input.is_action_just_released("ui_left"):
+		$Player.flip_h = 1
+		$AnimationPlayer.play("Stand")
+
