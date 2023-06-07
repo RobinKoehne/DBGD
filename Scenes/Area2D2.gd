e5feca4ae8d763dfb6f0ef2e6f478027f7e43988
goncalves-henrique <henrique.goncalves.001@student.uni.lu>
2023-06-07 09:24:07 +0200

    End screen hinzugefügt

--- Scenes/Area2D2.gd
+++ Scenes/Area2D2.gd
@@ -3,13 +3,11 @@ extends Area2D
 var entered = false
 
 func _on_body_entered(body: PhysicsBody2D):
-	entered = true 
+	entered = true
 
 func _on_body_exited(body):
 	entered = false
 	
 func _process(delta):
 	if entered == true:
-		if Input.is_action_just_pressed("switch_scene"):
-			StageManager.changeState(StageManager.main)
-
+		get_tree().change_scene_to_file("res://Scenes/end_screen.tscn")
